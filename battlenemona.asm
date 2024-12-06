10cd2280c6eadd21480f14ee7111854251bdd703
LSM <164604304+lsmmega@users.noreply.github.com>
2024-12-06 20:53:40 +0800

    Add files via upload

---
+++ battlenemona.asm
@@ -0,0 +1,1384 @@
+Music_battlenemona:
+	channel_count 4
+	channel 1, Music_battlenemona_Ch1
+	channel 2, Music_battlenemona_Ch2
+	channel 3, Music_battlenemona_Ch3
+	channel 4, Music_battlenemona_Ch4
+
+Music_battlenemona_Ch1:
+	volume 7, 7
+	note_type 12, 11, 6
+	tempo 123
+	octave 3
+	duty_cycle 1
+	note A_, 2
+	rest 4
+	note G_, 2
+	rest 8
+	note G_, 2
+	rest 4
+	note F#, 2
+	rest 8
+	note A_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note F#, 2
+	rest 2
+	note D_, 2
+	rest 2
+	note C_, 2
+	rest 2
+	octave 2
+	note B_, 2
+	rest 2
+	note A_, 2
+	rest 2
+	octave 8
+.mainLoop:
+	octave 2
+	duty_cycle 0
+	volume_envelope 5, 6
+	note D_, 2
+	rest 1
+	octave 1
+	note D_, 1
+	rest 6
+	octave 2
+	note F#, 2
+	note A_, 1
+	note D_, 2
+	note C_, 1
+	note D_, 2
+	rest 1
+	octave 1
+	note D_, 1
+	rest 6
+	octave 2
+	note F#, 1
+	octave 1
+	note A_, 1
+	rest 1
+	octave 2
+	note D_, 2
+	note C_, 1
+	note D_, 2
+	rest 1
+	note D_, 1
+	rest 6
+	octave 1
+	note G_, 2
+	octave 2
+	note D_, 1
+	note G_, 1
+	octave 1
+	note B_, 2
+	octave 2
+	note D_, 2
+	rest 1
+	note D_, 1
+	rest 6
+	octave 1
+	note A#, 2
+	octave 2
+	note F_, 1
+	octave 1
+	note G_, 2
+	note A#, 1
+	octave 2
+	note D_, 2
+	octave 1
+	rest 1
+	note A_, 1
+	rest 1
+	octave 2
+	note D_, 2
+	note G_, 1
+	note F#, 1
+	note D_, 1
+	octave 1
+	note A_, 2
+	note D_, 2
+	octave 2
+	note C_, 2
+	note D_, 2
+	rest 1
+	octave 1
+	note A_, 1
+	rest 2
+	octave 2
+	note E_, 1
+	note G_, 3
+	note F#, 1
+	octave 1
+	note A_, 1
+	note G_, 1
+	octave 2
+	note C_, 2
+	note E_, 1
+	note D_, 2
+	rest 1
+	note D_, 2
+	octave 1
+	note G_, 1
+	note B_, 1
+	octave 2
+	note G_, 2
+	note D_, 3
+	rest 1
+	note G_, 1
+	octave 1
+	note A_, 1
+	note G_, 1
+	octave 2
+	note D_, 3
+	note D_, 1
+	rest 1
+	octave 1
+	note A_, 1
+	octave 2
+	note C_, 1
+	note G_, 1
+	note F#, 3
+	note F#, 1
+	rest 1
+	note D_, 1
+	note E_, 1
+	note D_, 1
+	note G#, 2
+	octave 1
+	note B_, 2
+	octave 2
+	note D_, 2
+	note G#, 2
+	note G_, 2
+	octave 1
+	note A#, 2
+	octave 2
+	note C#, 2
+	note G_, 2
+	note F#, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note A_, 2
+	note F#, 2
+	note F_, 2
+	note G#, 2
+	octave 3
+	note C_, 2
+	octave 2
+	note F_, 2
+	note E_, 2
+	octave 1
+	note G_, 2
+	note B_, 2
+	octave 2
+	note E_, 2
+	octave 1
+	note A_, 2
+	note E_, 2
+	note G_, 2
+	note A#, 2
+	octave 2
+	note D_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note A_, 2
+	note F#, 2
+	octave 1
+	note B_, 2
+	octave 2
+	note D#, 2
+	note G_, 2
+	octave 1
+	note B_, 2
+	octave 2
+	note G#, 2
+	octave 1
+	note G#, 2
+	rest 2
+	note G#, 2
+	octave 2
+	note G_, 2
+	octave 1
+	note G_, 2
+	octave 2
+	note G_, 2
+	octave 1
+	note G_, 2
+	octave 2
+	note F#, 2
+	octave 1
+	note F#, 2
+	octave 2
+	note F#, 2
+	octave 1
+	note F#, 2
+	octave 2
+	note F_, 2
+	octave 1
+	note F_, 2
+	octave 2
+	note F_, 2
+	octave 1
+	note F_, 2
+	octave 2
+	note E_, 2
+	note E_, 2
+	note E_, 2
+	note E_, 2
+	note E_, 2
+	note E_, 2
+	note E_, 2
+	note E_, 2
+	octave 1
+	note A_, 2
+	note A_, 2
+	note A_, 2
+	note A_, 2
+	note A_, 2
+	rest 1
+	octave 2
+	note E_, 2
+	note E_, 1
+	octave 1
+	note E_, 2
+.loop1:
+	note D_, 2
+	rest 4
+	note D_, 2
+	rest 8
+	sound_loop 4, .loop1
+	rest 2
+	octave 3
+	duty_cycle 1
+	volume_envelope 11, 6
+	note A_, 2
+	note G_, 2
+	note F#, 2
+	note E_, 2
+	rest 4
+	note A_, 2
+	rest 2
+	note A_, 2
+	note G_, 2
+	note F#, 2
+	note G_, 2
+	note E_, 2
+	note_type 6, 11, 6
+	pitch_slide 1, 4, E_
+	note A_, 1
+	octave 4
+	note E_, 7
+	octave 3
+	note_type 12, 11, 6
+	rest 2
+	note A_, 2
+	note G_, 2
+	note F#, 2
+	note E_, 2
+	octave 2
+	note A_, 2
+	rest 2
+	octave 3
+	note A_, 2
+	rest 2
+	note G_, 2
+	note F#, 2
+	note E_, 2
+	note D_, 2
+	note C#, 2
+	note D_, 2
+	note E_, 2
+	octave 1
+	volume_envelope 5, 6
+	duty_cycle 0
+	note G_, 2
+	octave 2
+	note D_, 1
+	note G_, 3
+	octave 1
+	note D_, 1
+	note G_, 1
+	rest 2
+	note G_, 2
+	note A#, 3
+	note A#, 1
+	note A_, 2
+	note E_, 1
+	octave 2
+	note A_, 1
+	octave 1
+	rest 2
+	note F_, 1
+	note A_, 3
+	note F_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note A_, 1
+	note F_, 2
+	octave 2
+	note D_, 1
+	octave 1
+	note A_, 4
+	note D_, 2
+	note A_, 1
+	note G_, 1
+	note F_, 1
+	note D_, 1
+	note F_, 3
+	octave 2
+	note C_, 1
+	note F_, 2
+	octave 1
+	note A_, 1
+	octave 2
+	note F_, 1
+	octave 1
+	note D_, 1
+	note D_, 1
+	note F_, 2
+	note A_, 2
+	note F_, 2
+	note G_, 2
+	rest 1
+	octave 2
+	note D_, 2
+	octave 1
+	rest 1
+	note G_, 2
+	note D_, 1
+	note G_, 2
+	note B_, 1
+	octave 2
+	note D_, 1
+	octave 1
+	note G_, 3
+	note A_, 2
+	octave 4
+	duty_cycle 2
+	note D_, 1
+	rest 1
+	note A_, 1
+	note D_, 1
+	rest 1
+	note A_, 1
+	rest 2
+	note A_, 1
+	octave 5
+	note D_, 2
+	octave 4
+	note A_, 1
+	note A_, 1
+	note G_, 1
+	octave 3
+	note A_, 2
+	rest 4
+	note G_, 2
+	rest 8
+	note G_, 2
+	rest 4
+	note F#, 2
+	rest 8
+	note A_, 2
+	rest 4
+	note G_, 2
+	rest 8
+	note A_, 2
+	rest 4
+	octave 4
+	note C_, 2
+	rest 8
+	octave 3
+	note A_, 2
+	rest 4
+	note G_, 2
+	rest 8
+	note G_, 2
+	rest 4
+	note F#, 2
+	rest 8
+	note A_, 2
+	rest 4
+	note G_, 2
+	rest 8
+	note A_, 2
+	rest 2
+	note F_, 12
+	octave 8
+	sound_loop 0, .mainLoop
+
+Music_battlenemona_Ch2:
+	note_type 12, 11, 6
+	octave 4
+	duty_cycle 1
+	note D_, 2
+	rest 4
+	note C_, 2
+	rest 8
+	octave 3
+	note B_, 2
+	rest 4
+	note A_, 2
+	rest 8
+	octave 4
+	note D_, 2
+	rest 2
+	note C_, 2
+	octave 3
+	rest 2
+	note B_, 2
+	rest 2
+	note A_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note F#, 2
+	rest 2
+	note E_, 2
+	rest 2
+	note D_, 2
+	rest 2
+	octave 8
+.mainLoop:
+	octave 3
+	volume_envelope 5, 6
+	note D_, 16
+	note D_, 16
+	note D_, 16
+	note D_, 16
+	note D_, 16
+	note D_, 16
+	note D_, 16
+	note E_, 16
+	octave 2
+	note B_, 4
+	rest 4
+	note A#, 4
+	rest 4
+	note A_, 4
+	rest 4
+	note G#, 4
+	rest 4
+	note B_, 4
+	rest 4
+	note B_, 4
+	rest 4
+	octave 3
+	note C#, 4
+	rest 4
+	note D_, 2
+	rest 2
+	note D_, 4
+	note D_, 4
+	note D_, 4
+	note D_, 4
+	note D_, 4
+	note C#, 4
+	note C#, 4
+	note C_, 4
+	note C_, 4
+	octave 2
+	note B_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	note A#, 2
+	rest 2
+	note A#, 2
+	rest 2
+	note A#, 2
+	rest 2
+	note A#, 2
+	rest 2
+	octave 3
+	note F#, 2
+	rest 14
+	note F_, 2
+	rest 14
+	note G_, 2
+	rest 14
+	note G_, 2
+	rest 16
+	octave 4
+	volume_envelope 11, 6
+	note D_, 2
+	note C_, 2
+	octave 3
+	note B_, 2
+	note A_, 2
+	rest 4
+	octave 4
+	note D_, 2
+	rest 2
+	note D_, 2
+	note C_, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note C_, 2
+	octave 3
+	note A_, 2
+	note B_, 4
+	octave 4
+	rest 2
+	note D_, 2
+	note C_, 2
+	octave 3
+	note B_, 2
+	note A_, 2
+	note D_, 2
+	rest 2
+	octave 4
+	note D_, 2
+	rest 2
+	note C_, 2
+	octave 3
+	note B_, 2
+	note A_, 2
+	note G_, 2
+	note F#, 2
+	note G_, 2
+	note A_, 2
+	volume_envelope 5, 6
+	note G_, 2
+	rest 4
+	note G_, 2
+	rest 2
+	note G_, 2
+	rest 4
+	note A_, 2
+	rest 4
+	note A_, 2
+	rest 2
+	note A_, 2
+	rest 4
+	note A_, 2
+	rest 4
+	note A_, 2
+	rest 2
+	note F_, 2
+	rest 4
+	note F_, 2
+	rest 4
+	note F_, 2
+	rest 2
+	note F_, 2
+	rest 4
+	note C#, 2
+	rest 4
+	note C#, 2
+	rest 8
+	note C#, 2
+	octave 4
+	duty_cycle 1
+	note A_, 1
+	rest 1
+	octave 5
+	note D_, 1
+	octave 4
+	note A_, 1
+	rest 1
+	octave 5
+	note D_, 1
+	rest 2
+	note D_, 1
+	note F_, 2
+	note E_, 1
+	note D_, 1
+	note C_, 1
+	octave 4
+	volume_envelope 11, 6
+	note D_, 2
+	rest 4
+	note C_, 2
+	rest 8
+	octave 3
+	note B_, 2
+	rest 4
+	note A_, 2
+	rest 8
+	octave 4
+	note D_, 2
+	rest 4
+	note C_, 2
+	rest 8
+	note D_, 2
+	rest 4
+	note F_, 2
+	rest 8
+	note D_, 2
+	rest 4
+	note C_, 2
+	rest 8
+	octave 3
+	note B_, 2
+	rest 4
+	note A_, 2
+	rest 8
+	octave 4
+	note D_, 2
+	rest 4
+	note C_, 2
+	rest 8
+	note D_, 2
+	rest 2
+	note F_, 12
+	volume_envelope 11, 8
+	octave 8
+	sound_loop 0, .mainLoop
+
+Music_battlenemona_Ch3:
+	note_type 12, 1, 0
+	octave 2
+	note D_, 1
+	rest 2
+	note D_, 2
+	rest 1
+	octave 1
+	note A_, 1
+	rest 3
+	note A_, 1
+	rest 1
+	note B_, 1
+	rest 1
+	note A_, 1
+	rest 1
+	octave 2
+	note D_, 1
+	rest 2
+	note D_, 2
+	rest 1
+	octave 1
+	note A_, 1
+	rest 3
+	note A_, 1
+	rest 1
+	note B_, 1
+	note A_, 1
+	rest 1
+	note A_, 1
+	octave 2
+	note D_, 1
+	rest 1
+	octave 1
+	note D_, 1
+	octave 2
+	rest 1
+	note D_, 1
+	rest 1
+	octave 1
+	note D_, 1
+	octave 2
+	rest 1
+	note D_, 1
+	rest 1
+	octave 1
+	note D_, 1
+	octave 2
+	rest 1
+	note D_, 1
+	rest 1
+	octave 1
+	note D_, 1
+	rest 1
+	note D_, 1
+	rest 1
+	note D_, 1
+	rest 1
+	note D_, 1
+	rest 1
+	note D_, 1
+	rest 1
+	note D_, 1
+	rest 1
+	note D_, 1
+	rest 1
+	octave 2
+	note D_, 1
+	octave 1
+	note A_, 1
+	note D_, 1
+	note D_, 1
+	octave 8
+.mainLoop:
+	rest 2
+	octave 3
+	volume_envelope 1, 4
+	note A_, 2
+	octave 4
+	note D_, 2
+	octave 3
+	note A_, 1
+	rest 1
+	octave 4
+	note C#, 2
+	note D_, 1
+	rest 3
+	note A_, 3
+	rest 1
+	note D_, 2
+	note C#, 2
+	note D_, 1
+	rest 3
+	note E_, 1
+	rest 1
+	note F#, 2
+	note G_, 1
+	rest 1
+	note A_, 2
+	note D_, 1
+	rest 1
+	note D_, 1
+	octave 3
+	note B_, 1
+	octave 4
+	rest 1
+	note D_, 1
+	rest 2
+	note E_, 2
+	note D_, 2
+	note F_, 4
+	note E_, 2
+	note D_, 10
+	rest 4
+	octave 3
+	note A_, 2
+	octave 4
+	note_type 6, 1, 4
+	note C#, 1
+	note D_, 3
+	octave 3
+	note_type 12, 1, 4
+	note A_, 2
+	octave 4
+	note C#, 2
+	note D_, 1
+	rest 3
+	note A_, 4
+	note D_, 1
+	rest 1
+	note C#, 2
+	note D_, 1
+	rest 3
+	note E_, 1
+	rest 1
+	note F#, 2
+	note G_, 1
+	rest 1
+	note F#, 2
+	note D_, 1
+	rest 1
+	note D_, 1
+	octave 3
+	note B_, 1
+	octave 4
+	rest 1
+	note D_, 1
+	rest 2
+	note E_, 2
+	note D_, 1
+	rest 1
+	note F#, 2
+	rest 2
+	note G_, 2
+	note_type 6, 1, 4
+	note G#, 1
+	note A_, 3
+	note_type 12, 1, 4
+	note E_, 2
+	rest 2
+	note F#, 1
+	rest 1
+	note E_, 1
+	note D_, 3
+	octave 6
+	volume_envelope 1, 2
+	note D_, 4
+	octave 5
+	note A_, 2
+	note G_, 1
+	rest 1
+	note F_, 1
+	rest 1
+	note F#, 1
+	rest 1
+	note G_, 1
+	note A_, 1
+	rest 1
+	note B_, 1
+	rest 2
+	note D_, 1
+	rest 1
+	note E_, 1
+	rest 1
+	note F_, 3
+	note E_, 1
+	note D_, 3
+	octave 4
+	note_type 6, 1, 2
+	note B_, 1
+	note A_, 1
+	note G_, 1
+	note F_, 1
+	note D_, 1
+	rest 1
+	octave 5
+	note_type 6, 1, 2
+	note C#, 1
+	note D_, 3
+	octave 4
+	note_type 12, 1, 2
+	note B_, 1
+	rest 1
+	note A_, 2
+	octave 5
+	note D_, 1
+	rest 1
+	octave 4
+	note B_, 1
+	rest 1
+	note A_, 1
+	rest 1
+	octave 5
+	note F_, 1
+	note E_, 1
+	rest 1
+	note D_, 1
+	rest 2
+	note F_, 1
+	rest 1
+	note E_, 1
+	rest 1
+	note D_, 1
+	rest 1
+	note F#, 2
+	note G_, 1
+	rest 1
+	note A_, 1
+	note D_, 1
+	rest 1
+	note D_, 1
+	rest 2
+	octave 6
+	note D_, 2
+	octave 5
+	note G#, 1
+	note A_, 1
+	rest 2
+	note G_, 1
+	rest 1
+	note F#, 1
+	rest 1
+	note G_, 1
+	note A_, 1
+	note B_, 2
+	note A_, 1
+	rest 1
+	note D_, 1
+	rest 1
+	note E_, 1
+	note D_, 1
+	rest 1
+	octave 4
+	note A_, 1
+	octave 5
+	note D_, 1
+	note C#, 1
+	note D_, 2
+	note D_, 1
+	note E_, 1
+	note F#, 1
+	note G_, 1
+	note A_, 1
+	note A_, 1
+	note A_, 2
+	note A_, 1
+	note A_, 1
+	note A_, 2
+	note A_, 1
+	note A_, 1
+	note A_, 2
+	note A_, 1
+	note A_, 1
+	octave 6
+	note D_, 2
+	rest 1
+	note D_, 1
+	note D_, 2
+	note D_, 1
+	note D_, 1
+	note D_, 2
+	note F_, 1
+	note E_, 1
+	note D_, 4
+	note_type 4, 1, 2
+	note C_, 1
+	octave 5
+	note B_, 1
+	note A_, 1
+	note G_, 1
+	note F_, 1
+	note E_, 1
+	note_type 12, 1, 2
+	rest 2
+	volume_envelope 1, 4
+	note D_, 2
+	note C_, 2
+	octave 4
+	note B_, 2
+	note A_, 2
+	rest 4
+	octave 5
+	note D_, 2
+	rest 1
+	note_type 6, 1, 4
+	rest 1
+	note C#, 1
+	note_type 12, 1, 4
+	note D_, 2
+	note C_, 2
+	octave 4
+	note B_, 2
+	note A_, 2
+	rest 8
+	octave 5
+	note D_, 2
+	note C_, 2
+	octave 4
+	note B_, 2
+	note A_, 2
+	rest 4
+	octave 5
+	note D_, 2
+	rest 2
+	octave 4
+	note D_, 2
+	note F_, 2
+	note D_, 2
+	note F#, 2
+	note G_, 2
+	note A_, 2
+	note D_, 2
+	octave 1
+	volume_envelope 1, 0
+	note D_, 2
+	rest 4
+	note D_, 2
+	rest 8
+	note D_, 2
+	rest 4
+	note D_, 2
+	rest 8
+	note D_, 2
+	rest 4
+	note D_, 2
+	rest 8
+	note D_, 2
+	rest 4
+	note D_, 2
+	rest 10
+	octave 3
+	volume_envelope 1, 4
+	note A_, 2
+	octave 4
+	note D_, 2
+	octave 3
+	note A_, 2
+	octave 4
+	note C_, 2
+	note D_, 2
+	rest 2
+	note A_, 4
+	note D_, 2
+	note E_, 1
+	note D_, 1
+	rest 1
+	note D_, 1
+	note F_, 1
+	note F_, 1
+	note E_, 2
+	note D_, 1
+	note C_, 1
+	rest 1
+	note D_, 7
+	octave 5
+	note D_, 16
+	rest 12
+	octave 3
+	note A_, 2
+	octave 4
+	note D_, 1
+	octave 5
+	note D_, 1
+	octave 3
+	note A_, 2
+	octave 4
+	note C#, 2
+	note D_, 2
+	rest 2
+	note A_, 2
+	rest 16
+	octave 2
+	volume_envelope 1, 0
+	note D_, 1
+	rest 2
+	note D_, 2
+	rest 1
+	octave 1
+	note A_, 2
+	rest 2
+	note A_, 1
+	rest 1
+	note B_, 2
+	note A_, 1
+	octave 2
+	rest 1
+	note D_, 1
+	rest 1
+	note D_, 2
+	rest 2
+	octave 1
+	note A_, 1
+	rest 3
+	note A_, 2
+	octave 2
+	note D_, 1
+	note C_, 1
+	octave 1
+	note A_, 2
+	note G_, 1
+	rest 2
+	note G_, 2
+	note C#, 2
+	note G_, 3
+	note C#, 2
+	rest 1
+	note G_, 1
+	note B_, 1
+	note G_, 1
+	note A_, 3
+	octave 2
+	note D_, 1
+	octave 1
+	rest 1
+	note D_, 1
+	rest 1
+	note A_, 2
+	rest 2
+	note A_, 3
+	note E_, 1
+	octave 2
+	note C_, 1
+	note D_, 1
+	rest 2
+	note D_, 3
+	octave 1
+	note A_, 2
+	rest 1
+	note D_, 3
+	note B_, 2
+	note A_, 1
+	rest 1
+	octave 2
+	note D_, 1
+	rest 2
+	note D_, 2
+	rest 1
+	octave 1
+	note A_, 1
+	rest 2
+	note D_, 3
+	octave 2
+	note D_, 1
+	note C_, 1
+	octave 1
+	note A_, 2
+	note G_, 1
+	rest 2
+	note D_, 3
+	note F#, 3
+	octave 2
+	note D_, 1
+	note C_, 2
+	octave 1
+	note B_, 1
+	note A_, 1
+	note G_, 2
+	note A_, 1
+	rest 3
+	note A_, 12
+	octave 8
+	sound_loop 0, .mainLoop
+
+Music_battlenemona_Ch4:
+	toggle_noise 0
+	drum_speed 12
+	toggle_noise
+	toggle_noise 3
+.loop1:
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 1
+	octave 8
+	drum_note 1, 2
+	sound_loop 3, .loop1
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 1
+	drum_note 1, 1
+	drum_note 1, 1
+	drum_note 1, 1
+	drum_note 1, 1
+	drum_note 1, 1
+.mainLoop:
+	sound_call .sub1
+	drum_note 7, 1
+	rest 2
+	drum_note 7, 1
+	drum_note 1, 1
+	rest 1
+	drum_note 7, 1
+	rest 1
+	drum_note 7, 1
+	drum_note 1, 1
+	drum_note 7, 1
+	rest 2
+	drum_note 7, 1
+	drum_note 1, 1
+	rest 1
+	sound_call .sub1
+	octave 8
+	drum_speed 12
+	drum_note 7, 1
+	rest 2
+	drum_note 7, 1
+	drum_note 1, 1
+	rest 1
+	drum_note 7, 1
+	rest 1
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 1
+	rest 1
+	drum_note 7, 1
+	rest 1
+	drum_note 7, 1
+	rest 1
+	drum_note 7, 2
+	drum_note 7, 2
+	drum_note 1, 4
+	rest 2
+	drum_note 7, 2
+	drum_note 1, 4
+	drum_note 7, 2
+	drum_note 7, 2
+	drum_note 1, 2
+	drum_note 7, 2
+	rest 2
+	drum_note 7, 2
+	drum_note 1, 4
+	drum_note 7, 2
+	drum_note 7, 2
+	drum_note 1, 4
+	rest 2
+	drum_note 7, 2
+	drum_note 1, 4
+	drum_note 7, 2
+	drum_note 7, 2
+	drum_note 1, 2
+	drum_note 7, 2
+	rest 2
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 1, 2
+	drum_note 7, 2
+	drum_note 1, 2
+	drum_note 7, 2
+	drum_note 1, 4
+	drum_note 1, 2
+	drum_note 7, 2
+	drum_note 1, 4
+	drum_note 1, 2
+	drum_note 7, 2
+	drum_note 1, 2
+	drum_note 7, 2
+	drum_note 1, 2
+	drum_note 7, 2
+	drum_note 1, 1
+	drum_note 7, 2
+	drum_note 7, 1
+	drum_note 7, 1
+	rest 3
+	drum_note 1, 2
+	drum_note 7, 2
+	drum_note 7, 2
+	drum_note 1, 2
+	drum_note 7, 2
+	drum_note 1, 2
+	drum_note 7, 2
+	drum_note 1, 1
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 1, 1
+	drum_note 1, 1
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 7, 1
+.loop2:
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 2
+	sound_loop 8, .loop2
+.loop3:
+	drum_note 7, 1
+	rest 2
+	drum_note 7, 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 3
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 4
+	sound_loop 3, .loop3
+	drum_note 7, 1
+	rest 2
+	drum_note 7, 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 1
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 2
+	drum_note 7, 1
+	drum_note 1, 1
+	rest 1
+	drum_note 1, 1
+	drum_note 7, 1
+	rest 2
+	drum_note 7, 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 3
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 4
+	drum_note 7, 1
+	rest 9
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 1
+	drum_note 1, 1
+	drum_note 7, 1
+	rest 1
+.loop4:
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 2
+	sound_loop 7, .loop4
+	drum_speed 12
+	rest 1
+	drum_note 1, 1
+	rest 1
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 1, 1
+	drum_note 1, 1
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 1, 1
+	drum_note 1, 1
+	drum_note 1, 1
+	drum_note 1, 1
+	sound_loop 0, .mainLoop
+
+.sub1:
+	toggle_noise
+	toggle_noise 3
+	drum_speed 12
+	drum_note 7, 1
+	rest 2
+	drum_note 7, 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 3
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 1
+	drum_note 7, 1
+	rest 2
+	drum_note 7, 1
+	rest 2
+	drum_note 7, 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 3
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 1
+	drum_note 7, 1
+	rest 2
+	drum_note 7, 1
+	rest 2
+	drum_note 7, 1
+	drum_note 1, 2
+	drum_note 7, 1
+	rest 3
+	drum_note 7, 1
+	rest 1
+	drum_note 1, 1
+	drum_note 7, 1
+	rest 2
+	sound_ret
