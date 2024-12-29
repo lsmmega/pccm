91624649d71b8906e84fcf22e7b3f03f8b21479f
LSM <164604304+lsmmega@users.noreply.github.com>
2024-12-29 12:49:34 +0800

    Add files via upload

---
+++ inyourheart_lalala.asm
@@ -0,0 +1,768 @@
+Music_inyouheart_lalala:
+	channel_count 4
+	channel 1, Music_inyouheart_lalala_Ch1
+	channel 2, Music_inyouheart_lalala_Ch2
+	channel 3, Music_inyouheart_lalala_Ch3
+	channel 4, Music_inyouheart_lalala_Ch4
+
+Music_inyouheart_lalala_Ch1:
+	volume 7, 7
+	note_type 12, 6, 7
+	tempo 200
+	octave 3
+	note G_, 8
+	note F_, 8
+	note F_, 8
+	note G#, 8
+	note G_, 4
+	note D#, 4
+	note F_, 4
+	note D_, 4
+	octave 2
+	note A#, 2
+	note F_, 1
+	note A#, 1
+	rest 3
+	octave 1
+	note A#, 2
+	octave 2
+	sound_call .sub1
+	octave 3
+	note D_, 2
+	octave 2
+	note F_, 2
+	note D#, 8
+	note D#, 2
+	note F_, 2
+	note G_, 2
+	note A#, 2
+	octave 3
+	note A#, 8
+	note A#, 8
+	note G_, 8
+	note G_, 8
+	note D#, 8
+	note D#, 8
+	note D#, 4
+	rest 2
+	note F_, 2
+	note F_, 4
+	note A#, 4
+	octave 2
+	note C_, 2
+	note G_, 2
+	octave 3
+	note C_, 4
+	octave 1
+	note A#, 2
+	octave 2
+	note F_, 2
+	note A#, 4
+	octave 1
+	note G#, 2
+	octave 2
+	note D#, 2
+	note G#, 4
+	octave 1
+	note G_, 2
+	octave 2
+	note D#, 2
+	note G_, 4
+	octave 1
+	note F_, 2
+	octave 2
+	note C_, 2
+	note G#, 4
+	note D#, 2
+	note A#, 2
+	octave 3
+	note D#, 4
+	note C_, 2
+	octave 2
+	note G#, 2
+	octave 3
+	note C_, 4
+	octave 1
+	note G_, 2
+	octave 2
+	note D_, 2
+	note B_, 2
+	octave 3
+	note D_, 2
+	octave 1
+	note G#, 2
+	octave 2
+	note D#, 2
+	octave 3
+	note C_, 2
+	octave 2
+	note D#, 2
+	octave 1
+	note G_, 2
+	octave 2
+	note D#, 2
+	note A#, 2
+	note D#, 2
+	octave 1
+	note F_, 2
+	octave 2
+	note C_, 2
+	note G#, 2
+	note C_, 2
+	octave 1
+	note D#, 2
+	note A#, 2
+	octave 2
+	note G_, 2
+	octave 1
+	note A#, 2
+	note G#, 2
+	octave 2
+	note D#, 2
+	octave 3
+	note C_, 2
+	octave 2
+	note D#, 2
+	octave 1
+	note G_, 2
+	octave 2
+	note D#, 2
+	note A#, 2
+	note D#, 2
+	octave 1
+	note F_, 2
+	octave 2
+	note C_, 2
+	note F_, 2
+	octave 3
+	note F_, 4
+	octave 2
+	note F_, 2
+	note A_, 2
+	octave 3
+	note C_, 2
+	octave 1
+	note A#, 2
+	octave 2
+	note F_, 2
+	note A#, 2
+	note F_, 1
+	octave 1
+	note A#, 2
+	octave 2
+	sound_call .sub1
+	note_type 12, 6, 7
+	note A#, 1
+	octave 3
+	note C_, 1
+	note D_, 2
+	note D#, 8
+	rest 8
+	octave 8
+	sound_ret
+
+.sub1:
+	note A#, 1
+	note F_, 2
+	octave 1
+	note A#, 1
+	note F_, 1
+	rest 2
+	octave 2
+	note D#, 2
+	octave 1
+	note A#, 2
+	octave 2
+	note G_, 2
+	octave 1
+	note A#, 2
+	octave 2
+	note D_, 2
+	octave 1
+	note A#, 2
+	octave 2
+	note A#, 2
+	note D_, 2
+	note C_, 2
+	octave 1
+	note G_, 2
+	octave 2
+	note A#, 2
+	note C_, 2
+	octave 1
+	note A#, 2
+	octave 2
+	note F_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note F_, 2
+	octave 1
+	note G#, 2
+	octave 2
+	note D#, 2
+	octave 3
+	note C_, 2
+	octave 2
+	note D#, 2
+	octave 1
+	note G_, 2
+	octave 2
+	note D#, 2
+	note A#, 2
+	note D#, 2
+	octave 1
+	note F_, 2
+	octave 2
+	note C_, 2
+	octave 3
+	note C_, 2
+	octave 2
+	note F_, 2
+	octave 1
+	note A#, 2
+	octave 2
+	note F_, 2
+	note A#, 2
+	octave 1
+	note A#, 2
+	octave 2
+	note C_, 2
+	octave 1
+	note G_, 2
+	octave 2
+	note A#, 2
+	note C_, 2
+	octave 1
+	note A#, 2
+	octave 2
+	note F_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note F_, 2
+	octave 1
+	note G#, 2
+	octave 2
+	note D#, 2
+	octave 3
+	note C_, 2
+	octave 2
+	note D#, 2
+	octave 1
+	note G_, 2
+	octave 2
+	note D#, 2
+	note A#, 2
+	note D#, 2
+	octave 1
+	note F_, 2
+	octave 2
+	note C_, 2
+	octave 3
+	note C_, 2
+	octave 2
+	note F_, 2
+	octave 1
+	note G_, 2
+	octave 2
+	note D#, 2
+	note A#, 2
+	note D#, 2
+	octave 1
+	note G#, 2
+	octave 2
+	note D#, 2
+	octave 3
+	note C_, 2
+	octave 2
+	note D#, 2
+	octave 1
+	note A#, 2
+	octave 2
+	note F_, 2
+	sound_ret
+
+Music_inyouheart_lalala_Ch2:
+	note_type 12, 15, 8
+	rest 4
+	octave 4
+	duty_cycle 1
+	volume_envelope 8, 7
+	note G_, 4
+	note F_, 4
+	note A#, 4
+	note D#, 8
+	note D#, 4
+	note F_, 4
+	rest 4
+	note G_, 4
+	note F_, 4
+	note A#, 4
+	note D#, 4
+	octave 3
+	note D#, 1
+	note F_, 1
+	note D#, 1
+	note D_, 1
+	rest 3
+	sound_call .sub1
+	note G#, 2
+	note F_, 3
+	note D#, 1
+	note D_, 1
+	note D#, 8
+	rest 8
+	note D#, 2
+	note F_, 2
+	note G_, 2
+	note D#, 2
+	note D_, 4
+	note A#, 4
+	note C_, 2
+	note D_, 4
+	note D#, 2
+	note D#, 2
+	note C_, 2
+	octave 3
+	note A#, 2
+	note G_, 2
+	octave 4
+	note D#, 2
+	note F_, 2
+	note G_, 2
+	note D#, 2
+	octave 3
+	note A#, 4
+	octave 4
+	note G_, 4
+	note F_, 2
+	note C_, 2
+	note C_, 2
+	note D#, 2
+	note D_, 4
+	rest 2
+	note D#, 2
+	note D#, 2
+	note F_, 2
+	note G_, 2
+	note D#, 2
+	note D_, 4
+	note A#, 4
+	note D#, 2
+	note C_, 2
+	note C_, 2
+	note D#, 2
+	octave 3
+	note A#, 4
+	rest 2
+	note A#, 2
+	octave 4
+	note G#, 2
+	note G_, 2
+	note F_, 2
+	note D#, 2
+	note C_, 4
+	note G_, 2
+	note G_, 2
+	note F_, 2
+	note C_, 2
+	note C_, 2
+	note D#, 2
+	note D_, 4
+	rest 4
+	note D#, 1
+	note D_, 1
+	note C_, 1
+	note D_, 1
+	note D#, 2
+	octave 3
+	note A#, 1
+	note G#, 1
+	note G_, 1
+	note G#, 1
+	note A#, 2
+	note G_, 1
+	note G_, 1
+	note G#, 1
+	note A#, 1
+	note G#, 2
+	note G_, 1
+	note G#, 1
+	rest 1
+	note A#, 2
+	note G_, 2
+	note G_, 1
+	note G_, 2
+	rest 4
+	octave 4
+	note D#, 1
+	note D_, 1
+	note C_, 1
+	note D_, 1
+	note D#, 2
+	octave 3
+	note G_, 2
+	note G_, 1
+	note G#, 1
+	note A#, 2
+	rest 2
+	octave 4
+	note G_, 2
+	note A_, 8
+	rest 2
+	note A_, 2
+	note G_, 1
+	note A_, 2
+	note A_, 1
+	note A#, 4
+	octave 3
+	note D#, 3
+	note D_, 2
+	rest 2
+	sound_call .sub1
+	note_type 12, 8, 7
+	octave 4
+	note G#, 1
+	note G#, 1
+	note F_, 3
+	note D#, 1
+	note D_, 1
+	note D#, 8
+	octave 8
+	rest 8
+	sound_ret
+
+.sub1:
+	note A#, 1
+	octave 4
+	note G_, 1
+	note F_, 2
+	note D#, 1
+	note D#, 2
+	octave 3
+	note A#, 1
+	octave 4
+	note D#, 2
+	note F_, 2
+	note G_, 1
+	octave 3
+	note A#, 2
+	rest 1
+	note A#, 1
+	octave 4
+	note G_, 1
+	note F_, 2
+	note D#, 1
+	note D#, 2
+	octave 3
+	note A#, 1
+	octave 4
+	note D#, 2
+	note F_, 2
+	note G_, 1
+	octave 3
+	note A#, 2
+	rest 1
+	octave 4
+	note A#, 1
+	note A#, 1
+	note A#, 2
+	note A#, 1
+	note D#, 2
+	note D#, 1
+	note D#, 1
+	note D#, 1
+	note D_, 2
+	note D#, 5
+	note D#, 1
+	note F_, 2
+	note G_, 1
+	note C_, 2
+	note G#, 1
+	note G_, 2
+	note F_, 1
+	note D#, 1
+	note D#, 1
+	note F_, 2
+	rest 1
+	octave 3
+	note A#, 1
+	octave 4
+	note G_, 1
+	note F_, 2
+	note D#, 1
+	note D#, 2
+	octave 3
+	note A#, 1
+	octave 4
+	note D#, 2
+	note F_, 2
+	note G_, 1
+	note D_, 2
+	octave 3
+	rest 1
+	note A#, 1
+	octave 4
+	note G_, 1
+	note F_, 2
+	note D#, 1
+	note D#, 2
+	octave 3
+	note A#, 1
+	octave 4
+	note D#, 2
+	note F_, 2
+	note G_, 1
+	octave 3
+	note A#, 2
+	rest 1
+	octave 4
+	note A#, 1
+	note A#, 1
+	note A#, 2
+	note A#, 1
+	note D#, 2
+	note D#, 1
+	note D#, 2
+	note D_, 2
+	note D#, 1
+	octave 3
+	note A#, 2
+	octave 4
+	rest 1
+	note D#, 1
+	note A#, 1
+	note A#, 2
+	note A#, 1
+	note D#, 2
+	note D#, 1
+	note D#, 1
+	rest 1
+	note F_, 2
+	note G_, 1
+	note D_, 1
+	sound_ret
+
+Music_inyouheart_lalala_Ch3:
+	note_type 12, 1, 0
+	octave 2
+	note F_, 2
+	rest 4
+	note F_, 2
+	rest 8
+	note F_, 2
+	rest 4
+	note F_, 2
+	rest 8
+	note F_, 2
+	rest 4
+	note F_, 2
+	rest 8
+	note F_, 2
+	rest 4
+	note F_, 2
+	rest 1
+	sound_call .sub1
+	rest 8
+	note F_, 2
+	rest 4
+	note F_, 2
+	rest 8
+	note F_, 2
+	rest 4
+	note F_, 2
+	note F_, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note F_, 2
+	rest 4
+	note F_, 2
+	rest 8
+	note F_, 2
+	rest 4
+	note F_, 2
+	rest 8
+	octave 3
+	note C_, 8
+	octave 2
+	note A#, 8
+	note G#, 8
+	note G_, 6
+	note F#, 2
+	note F_, 8
+	note D#, 8
+	note D_, 2
+	note C_, 2
+	octave 1
+	note A#, 2
+	note G#, 2
+	note G_, 2
+	octave 2
+	note D_, 2
+	note G_, 4
+	note G#, 6
+	rest 2
+	note G_, 6
+	rest 2
+	note F_, 6
+	rest 2
+	note D#, 6
+	rest 2
+	note G#, 6
+	rest 2
+	note G_, 6
+	rest 2
+	note F_, 6
+	rest 2
+	note F_, 6
+	note C_, 2
+	octave 1
+	note A#, 9
+	octave 2
+	sound_call .sub1
+	note_type 12, 1, 0
+	rest 8
+	octave 8
+	sound_ret
+
+.sub1:
+	note A#, 1
+	note A_, 1
+	note G#, 1
+	note G_, 1
+	note F#, 1
+	note F_, 1
+	note E_, 1
+	note D#, 8
+	note D_, 8
+	note C_, 8
+	octave 1
+	note A#, 8
+	note G#, 8
+	note G_, 8
+	note F_, 8
+	note A#, 8
+	octave 2
+	note C_, 8
+	octave 1
+	note A#, 8
+	note G#, 8
+	note G_, 8
+	note F_, 8
+	note G_, 8
+	note G#, 8
+	note A#, 8
+	octave 2
+	note D#, 8
+	sound_ret
+
+Music_inyouheart_lalala_Ch4:
+	drum_speed 12
+	toggle_noise 0
+	toggle_noise
+	toggle_noise 3
+.loop1:
+	toggle_noise
+	toggle_noise 3
+	drum_note 4, 1
+	rest 3
+	toggle_noise
+	toggle_noise 2
+	drum_note 8, 1
+	rest 7
+	drum_note 8, 1
+	rest 3
+	sound_loop 3, .loop1
+	toggle_noise
+	toggle_noise 3
+	drum_note 4, 1
+	rest 3
+	toggle_noise
+	toggle_noise 2
+	drum_note 8, 1
+	rest 2
+	toggle_noise
+	toggle_noise 3
+	drum_note 4, 1
+	rest 1
+	drum_note 4, 1
+	rest 6
+.loop3:
+	toggle_noise
+	toggle_noise 3
+	drum_note 4, 1
+	rest 3
+	toggle_noise
+	toggle_noise 2
+	drum_note 8, 1
+	rest 3
+	toggle_noise
+	toggle_noise 3
+	drum_note 4, 1
+	drum_note 4, 1
+	rest 2
+	toggle_noise
+	toggle_noise 2
+	drum_note 8, 1
+	rest 3
+	sound_loop 8, .loop3
+	toggle_noise
+	toggle_noise 3
+	drum_note 12, 2
+	rest 14
+.loop2:
+	toggle_noise
+	toggle_noise 3
+	drum_note 4, 1
+	rest 3
+	toggle_noise
+	toggle_noise 2
+	drum_note 8, 1
+	rest 3
+	toggle_noise
+	toggle_noise 3
+	drum_note 4, 1
+	rest 3
+	toggle_noise
+	toggle_noise 2
+	drum_note 8, 1
+	rest 3
+	sound_loop 12, .loop2
+	toggle_noise
+	toggle_noise 3
+	drum_note 4, 1
+	rest 3
+	toggle_noise
+	toggle_noise 2
+	drum_note 8, 1
+	rest 2
+	toggle_noise
+	toggle_noise 3
+	drum_note 4, 1
+	rest 1
+	drum_note 4, 1
+	rest 6
+.loop4:
+	drum_note 4, 1
+	rest 3
+	toggle_noise
+	toggle_noise 2
+	drum_note 8, 1
+	rest 3
+	toggle_noise
+	toggle_noise 3
+	drum_note 4, 1
+	drum_note 4, 1
+	rest 2
+	toggle_noise
+	toggle_noise 2
+	drum_note 8, 1
+	rest 3
+	sound_loop 8, .loop4
+	toggle_noise
+	toggle_noise 3
+	drum_note 12, 2
+	rest 14
+	sound_ret
